c88580eb290fad221c013767f0f3af4c6536ca07
spirite4059 <405978636@qq.com>
2017-09-07 00:19:03 +0800

    修改了一下首页的样式

---
+++ comicWeb/src/main/java/com/comic/web/controller/TestController.java
@@ -0,0 +1,137 @@
+
+package com.comic.web.controller;
+
+import com.comic.web.base.controller.BaseController;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.comic.web.bean.Article;
+import com.comic.web.service.ArticleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Controller
+@RequestMapping("/Test")
+public class TestController extends BaseController
+{
+    protected Logger logger=LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private ArticleService articleService;
+
+    @RequestMapping("/Article")
+    public String article(ModelMap modelMap,Integer articleId)
+    {
+        System.out.println("请求 article");
+        return "/test/article";
+    }
+
+
+    //***********增删改查
+    @RequestMapping("/queryList")
+    @ResponseBody				//page代表当前是第几页，rows代表每个页面显示多少数据
+    public PageInfo queryList(Integer page,Integer rows)
+    {
+        //获得固定的数据
+        logger.info(page+":"+rows);	//给些初始的数值
+        PageInfo<Article> pageInfo = new PageInfo<Article>(null);
+
+        try
+        {
+            PageHelper.startPage(page, rows);
+            List<Article> list = articleService.getList();
+            pageInfo = new PageInfo<Article>(list);
+        }
+        catch(Exception e)
+        {
+            logger.info(e.toString());
+        }
+        return pageInfo;  //返回的是对象列表
+    }
+
+
+    @RequestMapping ("/insert")
+    @ResponseBody
+    public Map<String,Object> save(Article article)
+    {
+        Map<String,Object>result=this.success(null);
+
+        try
+        {
+            articleService.insert(article);
+        }catch(Exception e)
+        {
+            result=this.error(e.getMessage());//把错误信息输出去
+        }
+        return result;
+    }
+
+    @RequestMapping ("/insertAndGetId")
+    @ResponseBody
+    public Map<String,Object> insertAndGetId (@RequestBody Article article)
+    {
+        Map<String,Object>result;
+
+        try{
+            Article a=new Article();
+            a.setArticleName(article.getArticleName());
+            a.setArticleList(article.getArticleList());
+
+            //logger.debug("输入的id"+a.getArticleId().toString());
+            //System.out.println("输入sdf的id:"+a.getArticleId().toString());
+            //在返回的时候会直接放到输入的文章上去
+            articleService.insertAndGetId(a);
+
+            //logger.info("输出的id"+a.getArticleId().toString());
+
+            Map<String,Object> input_data=new HashMap<String, Object>();
+            input_data.put("key",a.getArticleId());
+            result=this.success(input_data);
+        }catch(Exception e)
+        {
+            Map<String,Object> input_data=new HashMap<String, Object>();
+            result=this.error(e.getMessage());
+        }
+        return result;
+    }
+
+
+    @RequestMapping("/update")	//根据情况更新
+    @ResponseBody
+    public Map<String,Object> update(@RequestBody Article article)
+    {
+        //logger.info(article.toString());
+
+        Map<String,Object> result = this.success(null);
+        try{
+            articleService.update(article);
+        }catch(Exception e){
+            result = this.error(e.getMessage());
+        }
+        return result;
+    }
+
+
+    @RequestMapping("/delete")	//根据情况更新
+    @ResponseBody
+    public Map<String,Object> delete(int id)
+    {
+        Map<String,Object> result = this.success(null);
+        try{
+            articleService.deleteById(id);
+        }catch(Exception e){
+            result = this.error(e.getMessage());
+        }
+        return result;
+    }
+}
